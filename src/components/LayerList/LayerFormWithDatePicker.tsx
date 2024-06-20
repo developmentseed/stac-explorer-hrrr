e309099dc0c3e87e88670a6de99f5e7b8d7e2eee
Aimee Barciauskas <aimee@developmentseed.org>
2024-06-21 02:44:01 +0800

    Fix timezone issues with URLs and add comments

--- src/components/LayerList/LayerFormWithDatePicker.tsx
+++ src/components/LayerList/LayerFormWithDatePicker.tsx
@@ -15,7 +15,7 @@ function LayerFormWithDatePicker({ config, collection, updateLayer }: Props) {
   const [timeMin, timeMax] = collection.extent.temporal.interval[0];
   const minDate = useMemo(() => new Date(timeMin ? Date.parse(timeMin) : 0), [timeMin]);
   const maxDate = useMemo(() => new Date(timeMax ? Date.parse(timeMax) : getMostRecentUTC()), [timeMax]);
-  // TODO: can't figure out how not to default to local time zone, so the datepicker is on my local time zone but the slider starts on UTC.
+
   const [selectedDate, setSelectedDate] = useState<Date | undefined>(maxDate);
   const [selectedStartDate, setSelectedStartDate] = useState<Date>(maxDate);
   const [dateError, setDateError] = useState<string>('');
@@ -27,16 +27,19 @@ function LayerFormWithDatePicker({ config, collection, updateLayer }: Props) {
     }
 
     setDateError('');
-    // on option is to use ISOString and replace the timezone with 00:00, but this doesn't work for the date picker, only the slider.
+
+    // the selected date will be in the local timezone, e.g. select "06/19/2024" will be midnight in the local timezone (of the users computer)
+    // This is a workaround to convert the date to midnight UTC by replacing the timezone offset with 00:00.
+    const isoString = date.toISOString();
+    const updatedIsoString = isoString.replace(/(\d{2}:\d{2})/, "00:00");
+    const utcDate = new Date(updatedIsoString);
 
     if (startDateChange) {
-      // the selected date will be in the local timezone, so we need to convert it to UTC.
-      const isoString = date.toISOString();
-      const updatedIsoString = isoString.replace(/(\d{2}:\d{2})/, "00:00");      
-      let utcDate = new Date(updatedIsoString);
+      // change both the start date and the selected date to midnight UTC.
       setSelectedDate(utcDate);
       setSelectedStartDate(utcDate);
     } else {
+      // if it's not a start date change, the slider correctly maintains the date in UTC.
       setSelectedDate(date);
     }
 
@@ -44,9 +47,9 @@ function LayerFormWithDatePicker({ config, collection, updateLayer }: Props) {
       ...config,
       renderConfig: {
         ...config.renderConfig,
-        datetime_str: date.toISOString(),
+        datetime_str: startDateChange ? utcDate.toISOString() : date.toISOString(),
         // if the start date has changed (from the date picker) use the date value, otherwise use the existing selected start date.
-        reference_dt_str: startDateChange ? date.toISOString() : selectedStartDate?.toISOString(),
+        reference_dt_str: startDateChange ? utcDate.toISOString() : selectedStartDate?.toISOString(),
       },
     });
   }, [config, updateLayer, selectedStartDate]);
@@ -55,8 +58,8 @@ function LayerFormWithDatePicker({ config, collection, updateLayer }: Props) {
     <FormControl isInvalid={!!dateError}>
       <FormLabel as="div">Select Date</FormLabel>
       <SingleDatepicker
-        // the start date is in UTC which will be converted to the date of the local time zone, so may appear off by one day.
-        // modify the selected date to be date +/- timezone offset, so the date in the picker matches UTC date in time slider
+        // The start date is in UTC which will be converted to the date of the local time zone, so may appear off by one day.
+        // Modify the selected date to be date +/- timezone offset, so the date in the picker matches UTC date in time slider.
         date={new Date(selectedStartDate.getTime() + selectedStartDate.getTimezoneOffset() * 60 * 1000)}
         onDateChange={(v) => onChange(v, true)}
         minDate={minDate}
@@ -76,7 +79,7 @@ function LayerFormWithDatePicker({ config, collection, updateLayer }: Props) {
           onChange={(d) => onChange(new Date(d))}
           onChangeEnd={(d) => onChange(new Date(d))}
         />
-      )}      
+      )}
       <FormErrorMessage>{dateError}</FormErrorMessage>
     </FormControl>
   );
